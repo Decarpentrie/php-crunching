d19aa57d33f01310e679e5408c30fc44738d8a2e
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-06-26 10:17:31 +0200

    exercice 1 et 2 dico

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
     $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
     $dico = explode("/n", $string);
-    // echo "$string";
+    // echo "$dico";
 
     // exercice dictionnaire
-    echo str_word_count($string);
+
+    // echo str_word_count($string);
+    if ($string <= 15) {
+        echo strlen($string);
+    }
 
 
 
